1775ddda1e825c1e2e86d300e81b95dc2ff0a55e
markakash <akashniki@gmail.com>
2024-07-10 10:26:15 +0000

    Spacewar: Use 8Gb dalvik heap size config
     - Helps fix java.lang.OutOfMemoryError: Failed to allocate a 24 byte
       allocation with 2582544 free bytes and 2522KB until OOM, target footprint 268435456, growth limit 268435456; giving up on allocation because <1% of heap free after GC

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
