13cc6b38175ada91374ef736bced7f9be37f9311
Michael Bestas <mkbestas@lineageos.org>
2025-02-23 03:00:52 +0200

    Spacewar: Eliminate system.prop
    Change-Id: Ia05f96ee124b6164f6b6d7d9b29622dd9abfbba5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,6 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # QCOM
