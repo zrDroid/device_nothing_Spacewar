85eb5072d1af2562f61c51c02cf4ee1bd73a8eab
MySelly <xusufgul@gmail.com>
2025-11-11 13:42:54 +0000

    spacewar: Enable blurs

--- lineage_spacewar.mk
+++ lineage_spacewar.mk
@@ -29,3 +29,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-nothing
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=Spacewar \
     BuildFingerprint="Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1666412462:user/release-keys"
+
+TARGET_ENABLE_BLUR := true
