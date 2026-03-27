e20313687c90bbfe192b8bbf5d4b638900253301
kleidione Freitas <kleidione@gmail.com>
2026-03-27 14:55:33 -0300

    Spacewar: vintf: Add missing Graphics HAL instances
    - 02-27 14:30:41.411   630   630 I servicemanager: Caller(pid=1218,uid=1000,sid=u:r:surfaceflinger:s0) Could not find android.hardware.graphics.composer3.IComposer/default in the VINTF manifest. No alternative instances declared in VINTF.
    - 02-27 14:30:41.458   630   630 I servicemanager: Caller(pid=1218,uid=1000,sid=u:r:surfaceflinger:s0) Could not find android.hardware.graphics.allocator.IAllocator/default in the VINTF manifest. No alternative instances declared in VINTF.
    - 02-27 14:30:41.461   630   630 I servicemanager: Caller(pid=1218,uid=1000,sid=u:r:surfaceflinger:s0) Could not find android.hardware.graphics.allocator.IAllocator/default in the VINTF manifest. No alternative instances declared in VINTF.
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,7 @@ TARGET_USES_FOD_ZPOS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
